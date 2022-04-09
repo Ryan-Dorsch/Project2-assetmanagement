b9d868ce454550fc398edc6f9aca7b99708fab35
Isabella  Hengler <isabella.hengler57@gmail.com>
2022-04-08 18:02:13 -0700

    Changed user files

--- src/main/java/com/revature/pms/repo/UsersRepository.java
+++ src/main/java/com/revature/pms/repo/UsersRepository.java
@@ -1,17 +1,21 @@
 package com.revature.pms.repo;
 
-import java.util.List;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.revature.pms.model.Users;
 
 
-public interface UsersRepository {
-	
-	public void addUsers(Users users);
-
-	public List<Users> listUsers();
-	
-	public Users getUsers(int userid);
-	
-	public void deleteUsers(Users users); 
+public interface UsersRepository extends JpaRepository<Users, Integer> {
 }
+
+	
+//	public void addUsers(Users users);
+//
+//	public List<Users> listUsers();
+//	
+//	public Users getUsers(int userid);
+//	
+//	public void deleteUsers(Users users); 
+//}
