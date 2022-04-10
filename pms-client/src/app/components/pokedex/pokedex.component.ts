8f06a02f5ea0f470df0928d3e03522efc8b17875
Dallas Doermann <dallas495@revature.net>
2022-04-09 21:50:57 -0700

    added the pokedex html

--- pms-client/src/app/components/pokedex/pokedex.component.ts
+++ pms-client/src/app/components/pokedex/pokedex.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pokedex',
-  templateUrl: './pokedex.component.html',
-  styleUrls: ['./pokedex.component.css']
+  templateUrl: 'pokedex.component.html',
+  styleUrls: ['pokedex.component.css']
 })
 export class PokedexComponent implements OnInit {
 
