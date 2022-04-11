6117077261df349bc5d99a22ae28c8f0b36024b5
Dallas Doermann <dallas495@revature.net>
2022-04-10 18:38:40 -0700

    signup

--- pms-client/src/app/app.module.ts
+++ pms-client/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
